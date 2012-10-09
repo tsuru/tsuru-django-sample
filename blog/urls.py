0c6851f5803520ea0280497d80ba813066ca4eb6
Francisco Souza <f@souza.cc>
2012-10-09 15:03:59 -0300

    all: added license information

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,7 @@
+# Copyright 2012 Globo.com. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
