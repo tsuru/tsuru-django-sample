3384f35625f728b3c1bf3ecbeac0ba68c0127905
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-03-15 11:07:45 -0300

    Set configurations for serving static files

--- blog/settings.py
+++ blog/settings.py
@@ -116,6 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = '/home/application/current/blog/static/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
