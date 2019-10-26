018321145fb26eff749dbe5b2df397bec8d59f97
John Victor <johnvictorio3@gmail.com>
2019-10-26 12:35:48 -0300

    [update] Post __str__

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=500)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
